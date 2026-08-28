a68041acdaa6641ac87b5aa3e3728392901bbd5a
usamafaheemAhmed <usamafaheemahmed80@gmail.com>
2026-08-28 11:24:45 +0500

    baseUrl

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Tailor-Project/", 
+  // base: "/Tailor-Project/", 
+  base: "/", 
   test: {
     environment: "jsdom", 
     globals: true,
